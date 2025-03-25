d85c60af906d11a4550df8db5eb7d302ffa20642
Hiiri-101 <fbiagentnumbor2@gmail.com>
2025-03-25 14:50:05 +0200

    other changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Kayttaja;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Note;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,5 +18,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         Kayttaja::factory(4)->create();
+
+        User::factory()->create([
+            'id' => 1,
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('pass123.')
+        ]);
+
+        Note::factory(100)->create();
     }
 }
