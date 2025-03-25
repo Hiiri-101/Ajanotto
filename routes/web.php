f40929cf45d984d2203b8ef4f8c74316ca32aee0
Hiiri-101 <fbiagentnumbor2@gmail.com>
2025-03-25 14:49:54 +0200

    NoteToRollback

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\CheckController;
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+/*
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+Route::post('/note', [NoteController::class, 'store'])->name('note.storage');
+Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+Route::post('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+*/
+
+Route::resource('note', NoteController::class);
 
 // Kirjautuminen
+/*
 Route::get('/', function () {
     return redirect('/login');
 });
+*/
 
 Route::get('/login', function () {
     return view('kirjautuminen.login');
